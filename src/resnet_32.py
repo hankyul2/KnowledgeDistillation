24f9412dee91b183bf52d03bfb44fd6711260a3b
hankyul <consistant1y@ajou.ac.kr>
2021-08-23 09:25:29 +0900

    refactor: Remove ResNet
    - Remove ResNet
    - Split ResNet32 from ResNet

--- src/resnet_32.py
+++ src/resnet_32.py
@@ -1,8 +1,51 @@
 from typing import Type, Union
 
 from torch import nn
+import torch.nn.functional as F
 
-from src.resnet import ResNet, BasicBlock, BottleNeck, conv1x1
+
+def conv1x1(in_channels, out_channels, stride=1):
+    return nn.Conv2d(in_channels, out_channels, kernel_size=(1, 1), stride=stride, bias=False)
+
+
+def conv3x3(in_channels, out_channels, stride=1, groups=1):
+    return nn.Conv2d(in_channels, out_channels, kernel_size=(3, 3), stride=stride, padding=1, bias=False, groups=groups)
+
+
+class BasicBlock(nn.Module):
+    factor = 1
+
+    def __init__(self, in_channels, out_channels, stride, norm_layer, downsample=None, groups=1, base_width=64):
+        super(BasicBlock, self).__init__()
+        self.conv1 = conv3x3(in_channels=in_channels, out_channels=out_channels, stride=stride)
+        self.conv2 = conv3x3(in_channels=out_channels, out_channels=out_channels)
+        self.bn1 = norm_layer(out_channels)
+        self.bn2 = norm_layer(out_channels)
+        self.downsample = downsample if downsample else nn.Identity()
+
+    def forward(self, x):
+        out = F.relu(self.bn1(self.conv1(x)))
+        return F.relu(self.downsample(x) + self.bn2(self.conv2(out)))
+
+
+class BottleNeck(nn.Module):
+    factor = 4
+
+    def __init__(self, in_channels, out_channels, stride, norm_layer, downsample=None, groups=1, base_width=64):
+        super(BottleNeck, self).__init__()
+        width = int(out_channels * (base_width / 64.0)) * groups
+        self.conv1 = conv1x1(in_channels, width)
+        self.conv2 = conv3x3(width, width, stride, groups=groups)
+        self.conv3 = conv1x1(width, out_channels * self.factor)
+        self.bn1 = norm_layer(width)
+        self.bn2 = norm_layer(width)
+        self.bn3 = norm_layer(out_channels * self.factor)
+        self.downsample = downsample if downsample else nn.Identity()
+
+    def forward(self, x):
+        out = F.relu(self.bn1(self.conv1(x)))
+        out = F.relu(self.bn2(self.conv2(out)))
+        return F.relu(self.downsample(x) + self.bn3(self.conv3(out)))
 
 
 class ResNet_32(nn.Module):
